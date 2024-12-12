210ee6038f61585d337fe9f8d5591e4863953eb3
a-gaoba <abdulrahman1997abdo@gmail.com>
2024-12-12 16:10:19 +0300

    init

--- backend/utils/generateLLMContent.js
+++ backend/utils/generateLLMContent.js
@@ -1,4 +1,4 @@
-const { GoogleGenerativeAI } = require("@google/generative-ai");
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
 export async function generateLLMContent(prompt) {
   try {
